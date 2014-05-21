9217b94fac6151f7cb591c2a611b524d745e2cc4
Jack Wu <wujack@Jacks-iMac.local>
2014-05-21 10:33:17 -0400

    Add views to the camera feed to demonstrate how to transform results from both metadata and features

--- Smerk Example/Smerk/SMKViewController.m
+++ Smerk Example/Smerk/SMKViewController.m
@@ -14,6 +14,9 @@
 
 @property SMKDetectionCamera *detector;
 
+@property (strong, nonatomic) UIView * faceFeatureTrackingView;
+@property (strong, nonatomic) UIView * faceMetaTrackingView;
+
 @end
 
 @implementation SMKViewController
@@ -21,17 +24,121 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self setupFaceTrackingViews];
     
     self.detector = [[SMKDetectionCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionFront];
+    
+    [self.detector setOutputImageOrientation:UIInterfaceOrientationPortrait];
+    [self.detector addTarget:self.cameraView];
+    
+    self.cameraView.fillMode = kGPUImageFillModePreserveAspectRatio;
+    
+    NSInteger outputHeight = [[self.detector.captureSession.outputs[0] videoSettings][@"Height"] integerValue];
+    NSInteger outputWidth = [[self.detector.captureSession.outputs[0] videoSettings][@"Width"] integerValue];
+    
+    if (UIInterfaceOrientationIsPortrait(self.detector.outputImageOrientation)) {
+        NSInteger temp = outputWidth;
+        outputWidth = outputHeight;
+        outputHeight = temp;
+    }
+
+    // Use self.view because self.cameraView is not resized at this point (if 3.5" device)
+    CGFloat previewHeight = self.view.frame.size.height;
+    CGFloat previewWidth = self.view.frame.size.width;
+    
+    /*
+     * Calculate the scale and offset of the preview vs the camera
+     */
+    CGFloat scale;
+    CGAffineTransform frameTransform;
+    switch (self.cameraView.fillMode) {
+        case kGPUImageFillModePreserveAspectRatio:
+            scale = MIN(previewWidth / outputWidth, previewHeight / outputHeight);
+            frameTransform = CGAffineTransformMakeScale(scale, scale);
+            frameTransform = CGAffineTransformTranslate(frameTransform, -(outputWidth * scale - previewWidth)/2, -(outputHeight * scale - previewHeight)/2 );
+            break;
+        case kGPUImageFillModePreserveAspectRatioAndFill:
+            scale = MAX(previewWidth / outputWidth, previewHeight / outputHeight);
+            frameTransform = CGAffineTransformMakeScale(scale, scale);
+            frameTransform = CGAffineTransformTranslate(frameTransform, -(outputWidth * scale - previewWidth)/2, -(outputHeight * scale - previewHeight)/2 );
+            break;
+        case kGPUImageFillModeStretch:
+            frameTransform = CGAffineTransformMakeScale(previewWidth / outputWidth, previewHeight / outputHeight);
+            break;
+    }
+    
+    CGAffineTransform RotationCompensationMatrix;
+    if (UIInterfaceOrientationIsPortrait(self.detector.outputImageOrientation)) {
+        // Interchange x & y
+        RotationCompensationMatrix = CGAffineTransformMake(0, 1, 1, 0, 0, 0);
+    }
+    else {
+        RotationCompensationMatrix = CGAffineTransformIdentity;
+    }
+
+ 
     [self.detector beginDetecting:kFaceFeatures | kMachineAndFaceMetaData
                         codeTypes:@[AVMetadataObjectTypeQRCode]
                withDetectionBlock:^(SMKDetectionOptions detectionType, NSArray *detectedObjects, CGRect clapOrRectZero) {
-                   NSLog(@"Detected objects %@", detectedObjects);
+                   if (detectedObjects.count) {
+                       NSLog(@"Detected objects %@", detectedObjects);
+                   }
+                   
+                   if (detectionType & kFaceFeatures) {
+                       if (!detectedObjects.count) {
+                           self.faceFeatureTrackingView.hidden = YES;
+                       }
+                       else {
+                           CIFaceFeature * feature = detectedObjects[0];
+                           CGRect face = feature.bounds;
+ 
+                           face = CGRectApplyAffineTransform(face, RotationCompensationMatrix);
+                           face = CGRectApplyAffineTransform(face, frameTransform);
+                           self.faceFeatureTrackingView.frame = face;
+                           self.faceFeatureTrackingView.hidden = NO;
+                       }
+                   }
+                   else if (detectionType | kFaceMetaData) {
+                       if (!detectedObjects.count) {
+                           self.faceMetaTrackingView.hidden = YES;
+                       }
+                       else {
+                           AVMetadataFaceObject * metadataObject = detectedObjects[0];
+                           
+                           CGRect face = metadataObject.bounds;
+                           
+                           // Flip the Y coordinate to compensate for coordinate difference
+                           face.origin.y = 1.0 - face.origin.y - face.size.height;
+                           
+                           // Transform to go from texels, which are relative to the image size to pixel values
+                           CGAffineTransform metaTexelToPixelTransform = CGAffineTransformMakeScale(outputWidth, outputHeight);
+                           face = CGRectApplyAffineTransform(face, RotationCompensationMatrix);
+                           face = CGRectApplyAffineTransform(face, metaTexelToPixelTransform);
+                           face = CGRectApplyAffineTransform(face, frameTransform);
+                           self.faceMetaTrackingView.frame = face;
+                           self.faceMetaTrackingView.hidden = NO;
+                       }
+                   }
                }];
     
-    [self.detector setOutputImageOrientation:UIInterfaceOrientationPortrait];
     [self.detector startCameraCapture];
-    [self.detector addTarget:self.cameraView];
+}
+
+- (void)setupFaceTrackingViews {
+    self.faceFeatureTrackingView = [[UIView alloc] initWithFrame:CGRectZero];
+    self.faceFeatureTrackingView.layer.borderColor = [[UIColor redColor] CGColor];
+    self.faceFeatureTrackingView.layer.borderWidth = 3;
+    self.faceFeatureTrackingView.backgroundColor = [UIColor clearColor];
+    self.faceFeatureTrackingView.hidden = YES;
+    
+    self.faceMetaTrackingView = [[UIView alloc] initWithFrame:CGRectZero];
+    self.faceMetaTrackingView.layer.borderColor = [[UIColor greenColor] CGColor];
+    self.faceMetaTrackingView.layer.borderWidth = 3;
+    self.faceMetaTrackingView.backgroundColor = [UIColor clearColor];
+    self.faceMetaTrackingView.hidden = YES;
+    
+    [self.cameraView addSubview:self.faceMetaTrackingView];
+    [self.cameraView addSubview:self.faceFeatureTrackingView];
 }
 
 - (IBAction)rotateCamera:(id)sender
